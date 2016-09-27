4ffbbd999fd33f951a672aa5b297c99096f3e3f4
Adri Van Houdt <adri.van.houdt@gmail.com>
2016-09-27 17:57:22 +0200

    Mine

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 console.log('Vladimir is the coolest (and the most modest) Node.js dev in the world');
-console.log('I am Adri and I approve this message')
+console.log('I am Adri and I "approve" this message')
